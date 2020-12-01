51a9b4077bc22d9a6cbe35f68e18fafbcab000fa
Gabriel Issamu Oda <biel.oda@gmail.com>
2020-12-01 10:57:27 -0300

    add generic programming activity

---
+++ Programação II/atividadePorgramaçãoGenérica/Num1/Main.java
@@ -0,0 +1,28 @@
+package capitulo13.genericType.exercicio.Num1;
+public class Main {
+    public static void main(String[] args) {
+        /* método criado para ser genérico: 
+        Pilha<**> p = new Pilha <**>();
+
+        ** = tipo de objeto (não pode ser tipos primitivos)
+        */
+
+        Pilha<Integer> p1 = new Pilha<Integer>();
+        
+        for(int i = 0; i < 10; i++){
+            p1.empilhar(i*10);
+        }
+
+        System.out.println(p1.toString());
+
+
+        Pilha<String> p = new Pilha<String>();
+        
+        for(int i = 0; i < 5; i++){
+            p.empilhar("bazinga");
+        }
+
+        System.out.println(p.toString());
+
+    }
+}
---
+++ Programação II/atividadePorgramaçãoGenérica/Num1/Pilha.java
@@ -0,0 +1,34 @@
+package capitulo13.genericType.exercicio.Num1;
+
+import java.util.ArrayList;
+
+public class Pilha <T>{
+    ArrayList<T> elementos;
+    
+    public Pilha(){
+        elementos = new ArrayList<T>(10);
+    }
+    
+    public void empilhar(T s){
+        elementos.add(s);
+    }
+    
+    public T desempilhar(){
+        if (elementos.isEmpty()){
+            return null;
+        }
+        T aux = elementos.get(elementos.size()-1);
+        elementos.remove(elementos.size()-1);
+        return aux;
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        for (T s : elementos){
+            sb.append(s);
+            sb.append(" | ");
+        }
+        return sb.toString();
+    }  
+}
