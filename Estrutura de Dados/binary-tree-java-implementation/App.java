ecb8ff97c555af0a1efe5991dda2c80c8c60b33a
Gabriel Issamu Oda <biel.oda@gmail.com>
2021-03-04 12:04:27 -0300

    add binary tree order result comment

--- Estrutura de Dados/binary-tree-java-implementation/App.java
+++ Estrutura de Dados/binary-tree-java-implementation/App.java
@@ -21,16 +21,19 @@ public class App {
         
         System.out.println("IN ORDER: ");
         binTree.inOrder(root);
+        // resultado: 1 - 2 - 5 - 6 - 7 - 10 - 15 - 20
         System.out.println();
         System.out.println();
 
         System.out.println("POST ORDER: ");
         binTree.postOrdem(root);
+        // resultado: 2 - 1 - 6 - 5 - 15 - 20 - 10 - 7 
         System.out.println();
         System.out.println();
 
         System.out.println("PRE ORDER: ");
         binTree.preOrder(root);
+        // resultado: 7 - 5 - 1 - 2 - 6 - 10 - 20 - 15 
         System.out.println();
         System.out.println();
 
